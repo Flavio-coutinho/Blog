4ff251bb74ff8c831c020c5006260524c9511615
Flavio Coutinho <coutinhoflavio20@gmail.com>
2022-07-29 16:15:01 -0300

    build: :building_construction: ad routes and cors

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+ const assets = {
+    images: {
+        logoDark: require('./images/Zuri1.png'),
+        appIcon: require('./images/icons8-rocket-48.png')
+    },
+    colors: {
+        secondary: '#292929'
+    }
+ }
+
+ export default assets
